dc18e2ab9981ebc1df2eefb24d20add1260af78c
zetaxalhaffar <info@versetech.net>
2024-01-31 01:26:28 +0200

    update index layout

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,11 @@
 <template>
   <div class="container-index my-6">
     <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 sm:gap-4">
-      <div class="col-span-1">
-        <ProfileCard />
-        <QRCode class="my-6" />
+      <div class="col-span-2 lg:col-span-1">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-4">
+          <ProfileCard />
+        <QRCode  />
+        </div>
       </div>
       <div class="col-span-2">
         <Products />
